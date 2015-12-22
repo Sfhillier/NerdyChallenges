8ccd8e1a5cde4ba3e4c46691681e25c128988a21
Scott Hillier <Sfhillier@gmail.com>
2015-12-22 00:00:53 -0500

    Finished working version.
    Needs cleaning up for readability but everything is working as intended.

--- NerdyChallenge/src/lizardmanGeneralRegisterOffice/LizardMan.java
+++ NerdyChallenge/src/lizardmanGeneralRegisterOffice/LizardMan.java
@@ -20,25 +20,45 @@ public class LizardMan {
 
 		String[] consonants = {"b","d","f","g","th","l","m","n","p","s","t","v","z","s","t","z"};
 		String[] vowels = {"a","e","i","o","u"};
-		String syllable = new String();
 		Random rand = new Random();
-		boolean isDone = false;
+		boolean hasNextSyllable = true;
 		String generatedName ="";
 		String[] listOfNames = new String[10];
-		int syllableCount = 0;
-		
+		int syllableCount = 1;
+		double probabilityNextSyllable = 0.6;
+
 		for(int i = 0; i < listOfNames.length; i++){
-			while(!isDone){
-				String consonantPart = consonants[rand.nextInt(consonants.length)];
-				String vowelPart = vowels[rand.nextInt(vowels.length)];
-				syllable = consonantPart + vowelPart;
-				generatedName+=syllable;
-				listOfNames[i]=generatedName;
-				isDone=true;
-				syllableCount++;
-			}
-			syllableCount = 0;
 			generatedName = "";
+			hasNextSyllable = true;
+			syllableCount = 0;
+			probabilityNextSyllable = 0.6;
+			while(hasNextSyllable){
+				String consonantPart = consonants[rand.nextInt(consonants.length)];
+				if(consonantPart == "s"){
+					double doubleS = rand.nextDouble();
+					if (doubleS > 0.5){
+						consonantPart = "ss";
+					}
+				}
+				generatedName += consonantPart;
+				if(syllableCount == 1){
+					generatedName= consonantPart.substring(0, 1).toUpperCase() + consonantPart.substring(1);
+				}
+				
+				String vowelPart = vowels[rand.nextInt(vowels.length)];
+				generatedName += vowelPart;
+				double randNextSyllable = rand.nextDouble();
+				if((syllableCount > 2) && randNextSyllable < probabilityNextSyllable){
+					hasNextSyllable = true;
+				}
+				else if((syllableCount >2) &&(randNextSyllable > probabilityNextSyllable)){
+					hasNextSyllable=false;
+				}
+				syllableCount++;
+				probabilityNextSyllable-=.1;
+			}
+			generatedName = generatedName.substring(0, generatedName.length()-1);
+			listOfNames[i]=generatedName;
 		}
 		for(int i = 0; i < listOfNames.length; i++){
 			System.out.println(listOfNames[i]);
